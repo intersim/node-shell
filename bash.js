d50a2ce0d9bdefc53726dba58d5e639eb17eebe1
intersim <emily.intersimone@gmail.com>
2016-01-19 18:11:26 -0500

    args now split on | symbol

--- bash.js
+++ bash.js
@@ -4,10 +4,16 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', function(data) {
   var input = data.toString().trim();
-	var cmd = input.split(" ")[0];
+  var cmd = input.split(" ")[0];
 
   if (input.indexOf(" ") > -1) {
     var args = input.split(" ").slice(1).join(" ");
+  }
+
+  if (input.indexOf('|') > -1) {
+    var cmdList = args.split(/\s*\|\s*/g);
+    args = args[0];
+    var otherCmds = args.slice(1);
   }
 
   try {
